146bd1b040ae664c4d07ce2ad8717a8f7a0c1dbd
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-20 15:00:37 +0300

    task01 added

---
+++ Task01/FibonacciNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    public static class FibonacciNumbers
+    {
+        /// <summary>
+        /// returns fibonacci numbers
+        /// </summary>
+        /// <param name="count">how many numbers you want to return</param>
+        /// <returns>IEnumerable fibonacci numbers</returns>
+        public static IEnumerable<int> Fibonacci(int count)
+        {
+            int first = 0, second = 1;
+            yield return second;
+            for (int i = 0; i < count; i++)
+            {
+                int temp = second;
+                second += first;
+                first = temp;
+                yield return second;
+            }
+        }
+    }
+}
