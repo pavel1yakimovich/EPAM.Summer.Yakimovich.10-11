02e017da98146426decc576a4a5fd5fcab964767
pavel1yakimovich <pavel1yakimovich@gmail.com>
2016-07-20 16:37:16 +0300

    task02 logic and tests added

--- Task02Logic/CustomQueue.cs
+++ Task02Logic/CustomQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,112 @@ using System.Threading.Tasks;
 
 namespace Task02Logic
 {
-    public class Class1
+    public class CustomQueue<T> : IEnumerable<T>
     {
+        public int Capacity => array.Length;
+
+        private  T[] array;
+
+        public CustomQueue()
+        {
+            array = new T[0];
+        }
+
+        public CustomQueue(T[] arr)
+        {
+            array = new T[arr.Length];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = arr[i];
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            T[] arrayNew = new T[array.Length + 1];
+            array.CopyTo(arrayNew, 0);
+
+            arrayNew[arrayNew.Length - 1] = item;
+            array = arrayNew;
+        }
+
+        public T Dequeue()
+        {
+            T item = array[0];
+
+            T[] arrayNew = new T[array.Length - 1];
+            int i = 0;
+            for (int j = 1; j < array.Length; j++, i++)
+                arrayNew[i] = array[j];
+            array = arrayNew;
+
+            return item;
+        }
+
+        public T Peek()
+        {
+            return array[0];
+        }
+
+        public void Clear()
+        {
+            array = new T[] { };
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
+        private T this[int currentIndex] => array[currentIndex];
+
+        public struct Enumerator : IEnumerator<T>
+        {
+            private readonly CustomQueue<T> collection;
+            private int currentIndex;
+
+            public Enumerator(CustomQueue<T> collection)
+            {
+                this.currentIndex = -1;
+                this.collection = collection;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    if (currentIndex == -1 || currentIndex == collection.Capacity)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                    return collection[currentIndex];
+                }
+            }
+
+            object System.Collections.IEnumerator.Current
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+                //throw new NotSupportedException();
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < collection.Capacity;
+            }
+
+            public void Dispose()
+            {
+            }
+        }
     }
 }
---
+++ Task02Tests/Task02LogicTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task02Logic;
+
+
+namespace Task02Tests
+{
+    public class Task02LogicTests
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new int[] { 1, 2 }, 5);
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void EnqueueAndDequeueTest(int[] array, int item)
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(array);
+            queue.Enqueue(item);
+            queue.Enqueue(item);
+            queue.Dequeue();
+            queue.Dequeue();
+            Assert.AreEqual(item, queue.Peek());
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void IteratorTest(int[] array, int result)
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(array);
+            int sum = 0;
+            foreach (int item in queue)
+                sum += item;
+            Assert.AreEqual(array.Sum(), sum);
+        }
+
+        [Test, TestCaseSource(nameof(TestCases))]
+        public void ClearTest(int[] array, int item)
+        {
+            CustomQueue<int> queue = new CustomQueue<int>(array);
+            queue.Clear();
+            queue.Enqueue(item);
+            Assert.AreEqual(item, queue.Peek());
+        }
+    }
+}
